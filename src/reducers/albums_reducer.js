029fde32607c664c47a014179b13febbf7b83bd5
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-26 00:04:10 -0500

    Create albumsReducer with sample initialState

---
+++ src/reducers/albums_reducer.js
@@ -0,0 +1,20 @@
+
+const initialState = {
+  1: {
+    name: 'Em Gai Mua',
+    cover_image: 'https://www.dropbox.com/s/rni2vqwqbodxjk9/EmGaiMua.jpg?dl=1',
+  },
+  2: {
+    name: 'Ve',
+    cover_image: 'https://www.dropbox.com/s/7mjkd2ta2d7fz4f/Ve.jpg?dl=1',
+  },
+};
+
+const albumReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default albumReducer;
