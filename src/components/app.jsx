8191562d2a70a628b97095329e7d54833e6d3302
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-26 00:36:02 -0500

    Create MainPage component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import { AuthRoute } from '../util/route_util';
 
-import MainPageContainer from './main/main_page_container';
+import MainPage from './main_page/main_page';
 
 const App = () => (
   <>
     <Switch>
-      <AuthRoute path="/" component={MainPageContainer} />
+      <AuthRoute path="/" component={MainPage} />
     </Switch>
   </>
 );
---
+++ src/components/main_page/main_page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import logo from './logo.svg';
+import './main_page.css';
+
+const MainPage = () => (
+  <div className="main-page">
+    <header className="main-page-header">
+      <h1>ZingLite</h1>
+      <img src={logo} className="main-page-logo" alt="logo" />
+    </header>
+  </div>
+);
+
+export default MainPage;
