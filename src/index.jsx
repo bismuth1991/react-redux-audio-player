2d9cfd2e97b3507b1d27844f6bd473997f47e0a8
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-26 00:42:54 -0500

    Create index.jsx entry point

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import configureStore from './store/store';
+import Root from './components/root';
+
+import './index.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  ReactDOM.render(
+    <Root store={store} />,
+    document.getElementById('root'),
+  );
+});
