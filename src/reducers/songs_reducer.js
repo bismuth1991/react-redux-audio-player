42a86975b8c137630965ab6d219fd3561caa8d56
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-26 00:08:25 -0500

    Add song urls to initialState in songsReducer

--- src/reducers/songs_reducer.js
+++ src/reducers/songs_reducer.js
@@ -5,21 +5,21 @@ const initialState = {
     title: 'Chia Tay Trong Mua',
     artist_id: 1,
     album_id: 1,
-    url: '',
+    url: 'https://www.dropbox.com/s/bplmds25ebqbyfg/ChiaTayTrongMua.mp3?dl=1',
   },
   2: {
     id: 2,
     title: 'Ve',
     artist_id: 2,
     album_id: 2,
-    url: '',
+    url: 'https://www.dropbox.com/s/asbuyjudiov3f2j/Ve.mp3?dl=1',
   },
   3: {
     id: 3,
     title: 'Bon Chu Lam',
     artist_id: 2,
     album_id: 2,
-    url: '',
+    url: 'https://www.dropbox.com/s/lio0f81hl8g2i8q/BonChuLam.mp3?dl=1',
   },
 };
 
