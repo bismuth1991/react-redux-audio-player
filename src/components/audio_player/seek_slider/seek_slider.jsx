5b431e37f8d3866dd3a3de52f765cd0993601350
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-26 19:40:56 -0500

    Add time display to SeekSlider component, basic styling

--- src/components/audio_player/seek_slider/seek_slider.jsx
+++ src/components/audio_player/seek_slider/seek_slider.jsx
@@ -2,24 +2,32 @@ import React from 'react';
 import { func, string } from 'prop-types';
 
 const SeekSlider = (props) => {
-  const { seekValue, handleSeek } = props;
+  const {
+    seekValue, handleSeek, currentAudioTime, totalAudioTime,
+  } = props;
 
   return (
-    <input
-      className="seek-slider"
-      type="range"
-      min="0"
-      max="100"
-      step="1"
-      value={seekValue}
-      onMouseUp={handleSeek}
-    />
+    <div className="seek-slider-wrapper">
+      <input
+        className="seek-slider"
+        type="range"
+        min="0"
+        max="100"
+        step="1"
+        value={seekValue}
+        onMouseUp={handleSeek}
+      />
+
+      <p>{`${currentAudioTime}/${totalAudioTime}`}</p>
+    </div>
   );
 };
 
 SeekSlider.propTypes = {
   seekValue: string.isRequired,
   handleSeek: func.isRequired,
+  currentAudioTime: string.isRequired,
+  totalAudioTime: string.isRequired,
 };
 
 export default SeekSlider;
