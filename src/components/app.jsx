05de5336766a677eb963934b8158a328d6612814
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-27 23:02:55 -0500

    Remove unecessary files and folders, slightly change width of slider class

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -11,13 +11,18 @@ import '../css/audio_player/slider.css';
 import '../css/audio_player/spinning_disc.css';
 import '../css/audio_player/marquee.css';
 import '../css/audio_player/audio_player_buttons.css';
-import '../css/audio_player/shuffle_button.css';
 import '../css/audio_player/seek_slider.css';
 import '../css/audio_player/volume_slider.css';
 
 const App = () => (
   <>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossOrigin="anonymous" />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+      integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
+      crossOrigin="anonymous"
+    />
+
     <Switch>
       <Route path="/" component={MainPage} />
     </Switch>
