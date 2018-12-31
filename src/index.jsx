f77416875fc50206ce9a55bc7ebd8d848bef7348
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-31 02:33:24 -0800

    Add rootSaga to configureStore and run it in index

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 
 import configureStore from './store/store';
 import Root from './components/root';
+import rootSaga from './sagas/audio_player_saga';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
+  store.runSaga(rootSaga);
 
   ReactDOM.render(
     <Root store={store} />,
--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
 import rootReducer from '../reducers/root_reducer';
 
-const configureStore = (preloadedState = {}) => (
-  createStore(
-    rootReducer,
-    preloadedState,
-    applyMiddleware(logger),
-  )
-);
+const configureStore = (preloadedState = {}) => {
+  const sagaMiddleware = createSagaMiddleware();
+  return {
+    ...createStore(rootReducer, preloadedState, applyMiddleware(sagaMiddleware, logger)),
+    runSaga: sagaMiddleware.run,
+  };
+};
+
 
 export default configureStore;
